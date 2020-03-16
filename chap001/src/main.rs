6957e352a8da5fefcf0e77d347d210145f8b058d
WATANAPEI <info@wpei.dev>
2020-03-17 01:03:02 +0900

    solved chap001

--- chap001/src/main.rs
+++ chap001/src/main.rs
@@ -2,5 +2,17 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    prinln!("{:?}", args);
+    println!("{:?}", args);
+
+    let num: i32 = args[1].parse().unwrap();
+    let mut count: i32 = 0;
+
+    for i in 1..num+1 {
+        //println!("{}", i);
+        if i % 3 == 0 || i % 5 == 0 {
+            count += i;
+        }
+    }
+    println!("count: {}", count);
+
 }
