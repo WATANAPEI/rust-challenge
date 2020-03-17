d0b7bf28cb96f0dfef3f5980d9660420778eda34
WATANAPEI <info@wpei.dev>
2020-03-18 00:49:04 +0900

    save

--- chap002/src/main.rs
+++ chap002/src/main.rs
@@ -1,6 +1,19 @@
 use std::env;
 
+fn gcd(a: i32, b: i32) -> i32 {
+    let mut gcd: i32 = 0;
+    return gcd;
+}
+
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    if args.len() != 3 {
+        println!("argument number must be 3");
+        return;
+    }
+    let a: i32 = args[1].parse().expect("arg1 must be integral");
+    let b: i32 = args[2].parse().expect("arg2 must be integral");
+    let gcd: i32 = gcd(a, b);
+    println!("gcd: {:?}", gcd);
 }
