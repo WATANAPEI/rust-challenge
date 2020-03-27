1f1080db449a7b62cfb8683d07af04b4df221713
WATANAPEI <info@wpei.dev>
2020-03-27 10:49:59 +0900

    add input sequence to ask ipaddress from and to

--- chap016/src/main.rs
+++ chap016/src/main.rs
@@ -5,22 +5,12 @@ use chap015::ipv4::IPv4;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("input ip v4 address:");
     println!("from: ");
-    let mut lines_iter = BufReader::new(std::io::stdin())
-        .lines().map(|l| l.unwrap());
-    match lines_iter.next() {
-        Some(v) => {
-            println!("echo: {}", v);
-        },
-        None => {
-            println!("Error occured: No input");
-        },
-    }
-
-    /*
-    for result in BufReader::new(std::io::stdin()).lines() {
-        let l = result?;
-        println!("{}", l);
-    }
-    */
+    let mut line = String::new();
+    let mut stdio_buffer = BufReader::new(std::io::stdin());
+    let _ = stdio_buffer.read_line(&mut line)?;
+    print!("echo: {}", line);
+    println!("to: ");
+    let _ = stdio_buffer.read_line(&mut line)?;
+    print!("echo: {}", line);
     Ok(())
 }
